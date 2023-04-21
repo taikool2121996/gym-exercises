e533a4711d37dbe5f7a67b29a10cc78a28f48ce7
taikool2121996 <taikool2121996@gmail.com>
2023-04-21 17:18:50 +0700

    setup app and make navbar

---
+++ src/pages/ExerciseDetail.js
@@ -0,0 +1,13 @@
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { Box } from '@mui/material';
+
+const ExerciseDetail = () => {
+  return (
+    <Box sx={{ mt: { lg: '96px', xs: '60px' } }}>
+      ExerciseDetail
+    </Box>
+  );
+};
+
+export default ExerciseDetail;
